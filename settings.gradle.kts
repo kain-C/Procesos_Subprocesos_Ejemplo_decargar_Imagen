34dbe77a14b06b06b397802412d463d8eb6d5e2c
kain-C- <92867625+kain-C@users.noreply.github.com>
2023-10-13 12:54:41 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Procesos_Subprocesos_Ejemplo_decargar_Imagen"
+include(":app")
+ 
